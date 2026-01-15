3f7ebe9bf45dec7f0486893c566c5ffd1dd4acbe
Vini Pasquini <64695549+Vini-Pasquini@users.noreply.github.com>
2026-01-15 05:17:47 -0300

    ph menu transition animation

--- Assets/Code/UIController.cs
+++ Assets/Code/UIController.cs
@@ -1,11 +1,20 @@
+using System.Collections;
 using UnityEngine;
 
 public class UIController : MonoBehaviour
 {
+    private enum UIPanel
+    {
+        None,
+        MainMenu,
+        Gameplay,
+    }
+
     [Header("Background Stuff")]
     [SerializeField] private Sprite _mainMenuSprite;
     [SerializeField] private Sprite _gameplaySprite;
-    [SerializeField] private SpriteRenderer _bgSpriteRenderer; // trocar pra Animator dps
+    //[SerializeField] private SpriteRenderer _bgSpriteRenderer; // trocar pra Animator dps
+    [SerializeField] private Animator _bgAnimator;
     [Header("UI Panels")]
     [SerializeField] private GameObject _mainMenuPanel;
     [SerializeField] private GameObject _gameplayPanel;
@@ -17,18 +26,17 @@ public class UIController : MonoBehaviour
     private void Start()
     {
         // menu setup
-        this._bgSpriteRenderer.sprite = this._mainMenuSprite;
-
-        this._mainMenuPanel.SetActive(true);
-        this._gameplayPanel.SetActive(false);
+        this._bgAnimator.SetBool("Zoom", false);
+        this._bgAnimator.Play("Zoomed Out");
+        
+        this.SetUIPanel(UIPanel.MainMenu);
     }
 
     public void OnPlayButtonPressed()
     {
-        this._bgSpriteRenderer.sprite = this._gameplaySprite;
-        
-        this._mainMenuPanel.SetActive(false);
-        this._gameplayPanel.SetActive(true);
+        this._bgAnimator.SetBool("Zoom", true);
+
+        this.StartCoroutine(WaitForBackgroundAnimation("Zoomed In"));
 
         for (int i = 0; i < GameManager.Instance.userList.Length;  i++)
         {
@@ -46,5 +54,23 @@ public class UIController : MonoBehaviour
     public void OnTurboButtonPressed()
     {
         Debug.Log("MODO TURBO!!!");
+    }
+
+    private void SetUIPanel(UIPanel targetUIPanel)
+    {
+        this._mainMenuPanel.SetActive(targetUIPanel == UIPanel.MainMenu);
+        this._gameplayPanel.SetActive(targetUIPanel == UIPanel.Gameplay);
+    }
+
+    private IEnumerator WaitForBackgroundAnimation(string targetAnimation)
+    {
+        this.SetUIPanel(UIPanel.None);
+
+        while (!this._bgAnimator.GetCurrentAnimatorStateInfo(0).IsName(targetAnimation))
+        {
+            yield return null;
+        }
+
+        this.SetUIPanel(targetAnimation == "Zoomed In" ? UIPanel.Gameplay : UIPanel.MainMenu);
     }
 }
